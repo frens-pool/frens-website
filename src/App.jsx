bed110b9efe49a0a664d4ec27bb6494c080cd11d
normadn <norman.saade@gmail.com>
2023-01-18 10:28:20 -1000

    use @vercel/analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,35 @@
-import React, { useEffect } from 'react';
-import {
-  Routes,
-  Route,
-  useLocation
-} from 'react-router-dom';
+import React, { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 
-import 'aos/dist/aos.css';
-import './css/style.css';
+import "aos/dist/aos.css";
+import "./css/style.css";
 
-import AOS from 'aos';
+import AOS from "aos";
+import { inject } from "@vercel/analytics";
 
-import Home from './pages/Home';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import ResetPassword from './pages/ResetPassword';
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ResetPassword from "./pages/ResetPassword";
 
 function App() {
-
   const location = useLocation();
 
   useEffect(() => {
+    inject();
+
     AOS.init({
       once: true,
-      disable: 'phone',
+      disable: "phone",
       duration: 700,
-      easing: 'ease-out-cubic',
+      easing: "ease-out-cubic",
     });
   });
 
   useEffect(() => {
-    document.querySelector('html').style.scrollBehavior = 'auto'
-    window.scroll({ top: 0 })
-    document.querySelector('html').style.scrollBehavior = ''
+    document.querySelector("html").style.scrollBehavior = "auto";
+    window.scroll({ top: 0 });
+    document.querySelector("html").style.scrollBehavior = "";
   }, [location.pathname]); // triggered on route change
 
   return (
