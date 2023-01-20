91f76e2af7927302f7d29c60b7aedd591bd6040f
normadn <norman.saade@gmail.com>
2023-01-20 11:35:51 -1000

    import ViteRadar

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import postcss from "./postcss.config.js";
 import react from "@vitejs/plugin-react";
+import ViteRadar from "vite-plugin-radar";
 
 // https://vitejs.dev/config/
 export default defineConfig({
