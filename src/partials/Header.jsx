5f757102d5474911a17f5c608c9904acca3cc8f2
normadn <norman.saade@gmail.com>
2023-01-26 13:37:57 -1000

    add discord link that doesn't expire

--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -51,7 +51,7 @@ function Header() {
             <ul className="flex flex-grow justify-end flex-wrap items-center">
               <li>
                 <a
-                  href="https://discord.gg/q8wxHBax"
+                  href="https://discord.gg/yx53KwrvRS"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3"
