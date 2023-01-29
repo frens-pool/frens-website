16a40914272eb21d313d72d78ee97d878119bdcb
normadn <norman.saade@gmail.com>
2023-01-28 17:30:34 -1000

    minor wording correction

--- src/partials/HeroHome.jsx
+++ src/partials/HeroHome.jsx
@@ -67,7 +67,7 @@ function HeroHome() {
                 data-aos="zoom-y-out"
                 data-aos-delay="150"
               >
-                Staking pool as a service protocol
+                Staking pools as a service
               </p>
               <div
                 className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center"
